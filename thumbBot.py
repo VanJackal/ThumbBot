17236a4613ac4ba045100f13d2fa84b6049a35e4
VanJackal <v@njackal.com>
2021-12-07 17:48:33 -0600

    basic message relay to channel

--- thumbBot.py
+++ thumbBot.py
@@ -1,4 +1,5 @@
 import discord
+from discord.channel import DMChannel
 from discord.ext import commands
 import yaml
 
@@ -7,10 +8,33 @@ with open("config.yaml") as f:
 with open("TOKEN") as f:
     TOKEN = f.read()
 
-bot = commands.Bot(command_prefix = config["prefix"])
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix = config["prefix"],intents = intents)
 
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot}")
+    print(f"Logged in as {bot.user}")
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    print("Message Recieved")
+    if type(message.channel) == DMChannel and getMemberGuilds(message.author):
+        print(message.content)
+        channel = bot.get_channel(config["channel"])
+        await channel.send(message.content)#TODO Format this message so its clear who sent it
+
+def getMemberGuilds(user):
+    commonGuilds = []
+    for guild in bot.guilds:
+        if user in guild.members:
+            member = guild.get_member(user.id)
+            role = guild.get_role(config['role'])
+            if role in member.roles:
+                commonGuilds.append(guild)
+    return commonGuilds
+
 
 bot.run(TOKEN)
