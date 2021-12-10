4a318a472b3860dd30911266b02773278d0df040
VanJackal <v@njackal.com>
2021-12-10 13:17:14 -0600

    Button placeholder

--- thumbBot.js
+++ thumbBot.js
@@ -5,7 +5,7 @@ const intents = ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MEMBERS', 'DIRECT_MESSAGES']
 const client = new Discord.Client({ intents: intents });
 
 const config = require("./config.json");
-const TOKEN = require("./token.json").TOKEN
+const TOKEN = require("./token.json").TOKEN;
 const PREFIX = config.prefix;
 
 logger.info("Startup")
@@ -15,13 +15,26 @@ client.on('ready', () => {
 });
 
 client.on('messageCreate', async message => {
-    if (message.author.id == client.user.id){//dont process messages from the bot
-        return
-    }
+    if (message.author.id == client.user.id) return;//dont process messages from the bot
+
     if (config.channelsSubmit.includes(message.channelId)) {//process messages sent in submit channels
         processSubmission(message)
-    }
+    }//TODO Verify user has the required role for this
 });
+
+client.on('messageCreate', async (message) => {
+    if (message.author.id == client.user.id || !message.guild) return;//dont process messages from the bot
+    member = await message.guild.members.fetch(message.author);
+
+    if (config.channelVerify == message.channelId && message.reference && member.roles.cache.has(config.adminRole)){
+        console.log(message.reference)
+        const channel = message.channel
+        const submission = await channel.messages.fetch(message.reference.messageId)
+        let comp = new Discord.MessageActionRow()
+        comp.addComponents(new Discord.MessageButton({label:"Verify",customId:"verifyButton",style:"PRIMARY"}))
+        submission.reply({content:`Verify the value \`${message.content}\` for this submission?`,components:[comp]})
+    }
+})
 
 const processSubmission = async (message) => {
     logger.debug(`Processing Submission - msgid:${message.id} content:\"${message.content}\"`)
